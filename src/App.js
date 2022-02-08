9c8fa4dcc32a23b1117a8d418b1b703b2c25c04a
Dimas <zhuravel16@gmail.com>
2022-02-08 23:44:02 +0200

    початок

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 import {useEffect} from "react";
 
 
-import User from "./components/User";
-import Posts from "./components/Posts";
-import Comment from "./components/Comment";
-
-
 function App() {
   return (
       <div>
-          <div className='wrap'>
-              <div className='block u'>{<User/>} </div>
-              <div className='block p'>{<Posts/>} </div>
-              <div className='block'>{<Comment/>} </div>
-
-          </div>
-
 
       </div>
   );
