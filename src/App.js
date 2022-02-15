881ff62457cad0de4150bf67a9fddbcbac261d47
Dimas <zhuravel16@gmail.com>
2022-02-15 19:34:39 +0200

    видалив файл

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import {useEffect} from "react";
 import Users from "./components/Users";
-import Details from "./components/Details";
 
 
 
@@ -10,7 +9,6 @@ function App() {
   return (
       <div>
         <div><Users/></div>
-        <div><Details/></div>
       </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -4,7 +4,9 @@ import React, {useEffect, useState} from 'react';
 import {userService} from "../services/UserServices";
 
 const Users = () => {
+
 const [users,setUsers] = useState([])
+
     useEffect( () => {
             userService.getAll()
             .then(value => {
@@ -14,16 +16,18 @@ const [users,setUsers] = useState([])
 
 
     let getUserId = (id)=>{
-        console.log(getAll)
+    console.log(id)
     }
+
     return (
         <div>
             <h1>USERS</h1>
             {
                 users.map(value =>
                     <div key={value.id}>{value.id} {value.name}
-                             <button className='btnGetDetails' onClick={()=>getDetails(value.id)}>Get Details</button>
+                             <button className='btnGetDetails' onClick={()=> getUserId(value.id)}>Get Details</button>
                     </div>)
+
             }
         </div>
     );
