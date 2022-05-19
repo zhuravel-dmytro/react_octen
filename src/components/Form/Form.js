e2c970e7c885d32bba998b412db5ca95a3ea6eff
Dimas <zhuravel16@gmail.com>
2022-05-19 21:11:05 +0300

    готове з кнопкою

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,14 +6,19 @@ const Form = ({ getFilter}) => {
     const formHandler = (e) => {
        const eventData= {...form, [e.target.name]:e.target.value}
         setForm({...form, ...eventData})
-        getFilter(eventData)
     }
+    const onSubmit = (e) =>{
+        e.preventDefault()
+        getFilter(form)
+    }
+
     return (
         <div>
             <form>
                 <label> Name:<input type="text" name={'name'} value={form.name} onChange={formHandler}/></label>
                 <label> User name:<input type="text" name={'username'} value={form.username} onChange={formHandler}/></label>
                 <label> Email:<input type="text" name={'email'} value={form.email} onChange={formHandler}/></label>
+                <button onClick={onSubmit}>Filter</button>
             </form>
         </div>
     );
